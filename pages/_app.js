67c64f7bdaf755d218c6a227c5443d9fd2e0d09b
Chenlili <986220912@qq.com>
2020-12-16 17:02:16 +0800

    把antd集成到项目中

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import 'antd/dist/antd.css'
 import '../styles/globals.less'
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import styles from '../styles/Home.module.less'
+import {Button} from 'antd'
+import React from "react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
+            <Button type='primary'>我是按钮，你是吗</Button>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
